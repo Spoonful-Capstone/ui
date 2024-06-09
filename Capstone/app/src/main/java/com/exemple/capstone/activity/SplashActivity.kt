5fd80a34d1fa75879517a4beba44c039601675fc
Husen <152364113+husenarif@users.noreply.github.com>
2024-06-09 20:51:51 +0700

    nambah ui

---
+++ Capstone/app/src/main/java/com/exemple/capstone/activity/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.exemple.capstone.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.exemple.capstone.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMainActivity()
+        }, 3000L)
+    }
+
+    private fun goToMainActivity() {
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+}
